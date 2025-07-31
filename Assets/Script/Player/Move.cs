f6ebecc5dceabd24cb80363f43c49e7cf3abd320
superbear1945 <2689505220@qq.com>
2025-07-31 15:29:24 +0800

    完成移动功能，移除报错的A*插件

---
+++ Assets/Script/Player/Move.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Move : MonoBehaviour
+{
+    PlayerInput _playerInput;
+    InputAction _moveAction;
+    Rigidbody2D _rb2d;
+    public float speed = 5f;
+
+    void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+        _moveAction = _playerInput.actions["Move"];
+        if (_moveAction == null)
+            Debug.LogError("No Move Action On" + gameObject.name);
+        if (_playerInput == null)
+            Debug.LogError("No PlayerInput Component On" + gameObject.name);
+
+        _rb2d = GetComponent<Rigidbody2D>();
+        if (_rb2d == null)
+            Debug.LogError("No Rigidbody2D Component On" + gameObject.name);
+    }
+
+
+    void FixedUpdate()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        Vector2 moveDirection = _moveAction.ReadValue<Vector2>();
+        _rb2d.MovePosition(_rb2d.position + moveDirection * Time.fixedDeltaTime * speed);
+    }
+
+}
