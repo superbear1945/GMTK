9648de9d59cd889a57bf858e1d993e938b05227f
superbear1945 <2689505220@qq.com>
2025-07-31 19:12:09 +0800

    实现按下左键生成正确旋转方向的子弹

--- Assets/Script/Player/Shoot.cs
+++ Assets/Script/Player/Shoot.cs
@@ -18,23 +18,29 @@ public class Shoot : MonoBehaviour
     void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
-        _shootAction = _playerInput.actions["Shoot"];
+        _shootAction = _playerInput.actions["ShootArm"];
         if (_playerInput == null)
             Debug.LogError("No PlayerInput Component On " + gameObject.name);
         if (_shootAction == null)
             Debug.LogError("No Shoot Action On " + gameObject.name);
 
         // 注册射击事件
-        _shootAction.performed += OnShoot;
+        _shootAction.performed += OnShootArm;
     }
 
     void OnDisable()
     {
-        _shootAction.performed -= OnShoot;  // 取消注册事件，避免内存泄漏
+        _shootAction.performed -= OnShootArm;  // 取消注册事件，避免内存泄漏
     }
 
-    void OnShoot(InputAction.CallbackContext context)
+    void OnShootArm(InputAction.CallbackContext context)
     {
+        bool isLeft = false;
+        if (context.control.name == "leftButton")
+            isLeft = true;
+        else if (context.control.name == "rightButton")
+            isLeft = false;
+        Debug.Log(isLeft);
         //获取玩家当前朝向
         Transform playerTransform = GameManager.CurrentPlayer.transform;
         Vector2 direction = playerTransform.right.normalized;
