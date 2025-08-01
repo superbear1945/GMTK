c8a60f1f37be590810742a6af664d93e8fae79cb
superbear1945 <2689505220@qq.com>
2025-08-01 19:05:09 +0800

    实现主角被摸到就重置关卡的功能，正在处理UI的TMP为正确赋值的bug

--- Assets/Script/Enemy/Enemy.cs
+++ Assets/Script/Enemy/Enemy.cs
@@ -5,5 +5,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.collider.name);
+        if (collision.collider.CompareTag("Player"))
+        {
+            // 如果敌人碰到玩家，结束游戏（暂定）
+            GameManager.Instance.GameEnd();
+        }
+    }
 }
--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // 确保引入SceneManagement命名空间
 
 public class GameManager : MonoBehaviour
 {
     static public GameManager Instance { get; private set; }
-    
+    public Transform spawnPoint;
+
     /// 当前玩家在GameObject的全局访问点（向后兼容）
-    static public GameObject CurrentPlayer 
-    { 
-        get 
-        { 
+    static public GameObject CurrentPlayer
+    {
+        get
+        {
             return Player.Instance != null ? Player.Instance.gameObject : null;
-        } 
+        }
     }
-    
+
+    public void GameEnd()
+    {
+        SceneManager.LoadScene("SampleScene");
+        CurrentPlayer.transform.position = spawnPoint.position; // 重置玩家位置到生成点
+    }
+
     private void Awake()
     {
         if (Instance == null)
@@ -27,7 +35,7 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    
+
     private void Start()
     {
         // 验证 Player 实例是否存在
@@ -39,5 +47,10 @@ public class GameManager : MonoBehaviour
         {
             Debug.LogWarning("场景中未找到Player实例！");
         }
+
+        if (spawnPoint == null)
+            Debug.LogError("Spawn Point未设置，请在GameManager中设置出生点");
+        
+        CurrentPlayer.transform.position = spawnPoint.position; // 确保玩家在游戏开始时位于生成点
     }
 }
