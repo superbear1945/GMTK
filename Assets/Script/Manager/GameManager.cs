e1eb29df144e41eb51e2e0b170f82427c41904be
superbear1945 <2689505220@qq.com>
2025-08-02 17:24:38 +0800

    修复GameManager的bug

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -39,9 +39,9 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        // 验证 Player 实例是否存在
-        if(Player.Instance != null)
-            Debug.LogWarning("场景中未找到Player实例！");
+        // // 验证 Player 实例是否存在
+        // if(Player.Instance != null)
+        //     Debug.LogWarning("场景中未找到Player实例！");
 
         if (spawnPoint == null)
             Debug.LogError("Spawn Point未设置，请在GameManager中设置出生点");
